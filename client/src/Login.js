6b0cbb919462260737d72d729a642bdb56dbdbcb
brayanbm15 <bracr7pro@gmail.com>
2024-09-05 16:42:23 -0500

    Descripción de los cambios realizados

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import './Login.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Footer from './components/footer';
-
+import {faLock} from '@fortawesome/free-solid-svg-icons'
+import {faUser} from '@fortawesome/free-solid-svg-icons'
 function Login() {
   return (
-    <div className="main-container"> {/* Div principal */}
+    <div className="container">
       <div className="login-container">
         <img 
-          src={require('assets/img/logo.png')} 
+          src={require('./assets/img/logo2.png')} 
           alt="Logo"
         />
         <h2>Login</h2>
         <form>
           <div className="placeholder-container">
             <div className="form-group">
+            <FontAwesomeIcon icon={faUser} fontSize={20} className='icons'></FontAwesomeIcon>
               <label htmlFor="username">Nombre:</label>
               <input 
                 type="text" 
@@ -23,6 +26,7 @@ function Login() {
               />
             </div>
             <div className="form-group">
+            <FontAwesomeIcon icon={faLock} fontSize={20} className='icons'></FontAwesomeIcon>
               <label htmlFor="password">Contraseña:</label>
               <input 
                 type="password" 
@@ -35,7 +39,7 @@ function Login() {
           <button className='button' type="submit">Enviar</button>
         </form>
         <div className="links"> {/* Links de registro y recuperación */}
-          <a href="/register">¿Aún no tienes cuenta? Regístrate</a>
+          <a href="/Register.js">¿Aún no tienes cuenta? Regístrate</a>
           <br />
           <a href="/forgot-password">Olvidaste tu contraseña?</a>
         </div>

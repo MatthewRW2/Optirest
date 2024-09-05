b52e6d7534098aed3176f337c2e93b04fd2cffeb
brayanbm15 <bracr7pro@gmail.com>
2024-09-04 22:11:36 -0500

    Descripción de los cambios realizados

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,11 +1,46 @@
 import React from 'react';
+import './Login.css';
+import Footer from './components/footer';
 
 function Login() {
   return (
-    <div>
-      <h2>Login</h2>
-      {/* Aquí puedes añadir el formulario de login */
-      PEPE}
+    <div className="main-container"> {/* Div principal */}
+      <div className="login-container">
+        <img 
+          src={require('assets/img/logo.png')} 
+          alt="Logo"
+        />
+        <h2>Login</h2>
+        <form>
+          <div className="placeholder-container">
+            <div className="form-group">
+              <label htmlFor="username">Nombre:</label>
+              <input 
+                type="text" 
+                id="username" 
+                placeholder="Ingrese su nombre" 
+                required 
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Contraseña:</label>
+              <input 
+                type="password" 
+                id="password" 
+                placeholder="Ingrese su contraseña" 
+                required 
+              />
+            </div>
+          </div>
+          <button className='button' type="submit">Enviar</button>
+        </form>
+        <div className="links"> {/* Links de registro y recuperación */}
+          <a href="/register">¿Aún no tienes cuenta? Regístrate</a>
+          <br />
+          <a href="/forgot-password">Olvidaste tu contraseña?</a>
+        </div>
+      </div>
+      <Footer /> {/* Usar el componente Footer aquí */}
     </div>
   );
 }

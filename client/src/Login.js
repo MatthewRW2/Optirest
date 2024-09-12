05d8b52ebbbd86baeeadfdfb6fa1b4e59cb18b3f
NicolasBarretoReina <93836101+NicolasBarretoReina@users.noreply.github.com>
2024-09-12 11:25:31 -0500

    Registro casi terminado y login con ajustes
    A

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Footer from './components/footer';
-import {faLock} from '@fortawesome/free-solid-svg-icons'
-import {faUser} from '@fortawesome/free-solid-svg-icons'
+import {faLock} from '@fortawesome/free-solid-svg-icons';
+import {faUser} from '@fortawesome/free-solid-svg-icons';
 
 function Login() {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  
+
   return (
     <div className="container">
       <div className="login-container">
@@ -20,6 +26,9 @@ function Login() {
             <FontAwesomeIcon icon={faUser} fontSize={20} className='icons'></FontAwesomeIcon>
               <label htmlFor="username">Nombre:</label>
               <input 
+                onChange={(event)=>{
+                  setUsername(event.target.value);
+                }}
                 type="text" 
                 id="username" 
                 placeholder="Ingrese su nombre" 
@@ -30,6 +39,9 @@ function Login() {
             <FontAwesomeIcon icon={faLock} fontSize={20} className='icons'></FontAwesomeIcon>
               <label htmlFor="password">Contraseña:</label>
               <input 
+                onChange={(event)=>{
+                  setPassword(event.target.value);
+                }}
                 type="password" 
                 id="password" 
                 placeholder="Ingrese su contraseña" 
@@ -40,9 +52,9 @@ function Login() {
           <button className='button' type="submit">Enviar</button>
         </form>
         <div className="links"> {/* Links de registro y recuperación */}
-          <a href="/Register.js">¿Aún no tienes cuenta? Regístrate</a>
+          <a href="/Register">¿Aún no tienes cuenta? Regístrate</a>
           <br />
-          <a href="/forgot-password">Olvidaste tu contraseña?</a>
+          <a href="/OlvidasteTuContraseña">Olvidaste tu contraseña?</a>
         </div>
       </div>
       <Footer /> {/* Usar el componente Footer aquí */}

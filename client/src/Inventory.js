f7234b58471965881a2e84af44e01512d5fbba5f
José Mateo López <josemateolopezcifuentes@gmail.com>
2024-09-14 14:42:55 -0500

    Estilos mejorados de inventario + funcion de barra de busqueda

--- client/src/Inventory.js
+++ client/src/Inventory.js
@@ -6,51 +6,63 @@ import './assets/css/Styles.css';
 const Inventory = () => {
   const [alimentos, setAlimentos] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
+  const [searchTrigger, setSearchTrigger] = useState(''); 
 
   useEffect(() => {
     fetch('http://localhost:3001/alimentos')
-      .then(response => response.json())
-      .then(data => setAlimentos(data))
-      .catch(error => console.error('Error al obtener alimentos:', error));
+      .then((response) => response.json())
+      .then((data) => setAlimentos(data))
+      .catch((error) => console.error('Error al obtener alimentos:', error));
   }, []);
 
+  // Función para manejar el cambio en el input de búsqueda
   const handleSearchChange = (event) => {
     setSearchTerm(event.target.value);
   };
 
-  const filteredAlimentos = alimentos.filter(alimento =>
-    alimento.nombreAlimento.toLowerCase().includes(searchTerm.toLowerCase())
+  // Función para ejecutar la búsqueda cuando se haga clic en el botón
+  const handleSearchSubmit = () => {
+    setSearchTrigger(searchTerm); // Se actualiza el trigger de búsqueda
+  };
+
+  // Filtrar los alimentos basado en el término de búsqueda
+  const filteredAlimentos = alimentos.filter((alimento) =>
+    alimento.nombreAlimento.toLowerCase().includes(searchTrigger.toLowerCase())
   );
 
   return (
     <div>
       <Navbar className="navbar-custom" />
       <div className="main-content">
-        <div className="inventory-search-custom">
-          <input
-            type="text"
-            placeholder="Buscar alimentos..."
-            className="search-input-custom"
-            value={searchTerm}
-            onChange={handleSearchChange}
-          />
-          <button className="action-button-custom action-button-danger-custom">Entrada de Alimentos</button>
-          <button className="action-button-custom">Actualizar Alimento</button>
-        </div>
-
-        {/* Botones de categorías */}
-        <div className="category-buttons-container">
-          <button className="category-button">Proteína</button>
-          <button className="category-button">Carbohidratos</button>
-          <button className="category-button">Legumbres</button>
-          <button className="category-button">Verduras</button>
-          <button className="category-button">Cereales</button>
-          <button className="category-button">Lácteos</button>
-          <button className="category-button">Bebidas</button>
-        </div>
-
         <div className="inventory-container-custom">
+          {/* Contenedor Izquierdo */}
           <div className="menu-left-unique-custom">
+            <div className="inventory-search-custom">
+              <input
+                type="text"
+                placeholder="Buscar alimentos..."
+                className="search-input-custom"
+                value={searchTerm}
+                onChange={handleSearchChange}
+              />
+              <button className="search-button-custom" onClick={handleSearchSubmit}>
+                <i className="fas fa-search"></i> 
+              </button>
+              <button className="action-button-custom action-button-danger-custom">Entrada de Alimentos</button>
+              <button className="action-button-custom">Actualizar Alimento</button>
+            </div>
+
+            {/* Botones de categorías */}
+            <div className="category-buttons-container">
+              <button className="category-button">Proteína</button>
+              <button className="category-button">Carbohidratos</button>
+              <button className="category-button">Legumbres</button>
+              <button className="category-button">Verduras</button>
+              <button className="category-button">Cereales</button>
+              <button className="category-button">Lácteos</button>
+              <button className="category-button">Bebidas</button>
+            </div>
+
             <div className="menu-left-container-unique-custom">
               <h2 className="menu-heading-unique-custom">Inventario de alimentos</h2>
               <table className="menu-table-unique-custom">
@@ -65,7 +77,7 @@ const Inventory = () => {
                 </thead>
                 <tbody>
                   {filteredAlimentos.length > 0 ? (
-                    filteredAlimentos.map(alimento => (
+                    filteredAlimentos.map((alimento) => (
                       <tr key={alimento.IdAlimento}>
                         <td>{alimento.nombreAlimento}</td>
                         <td>{alimento.IdCategoria}</td>
@@ -86,6 +98,8 @@ const Inventory = () => {
               </table>
             </div>
           </div>
+
+          {/* Contenedor Derecho */}
           <div className="menu-right-unique-custom">
             <div className="right-box-unique-custom">
               <h2 className="menu-heading-unique-custom">Resumen de inventario</h2>

a41022b22907ac83a0a0a8d44603eff836871629
Linxidrf <dayanaidrf@gmail.com>
2024-09-13 20:07:43 -0500

    Estilos arreglados

--- client/src/UserRegister.js
+++ client/src/UserRegister.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserGroup, faUserGraduate, faClock } from '@fortawesome/free-solid-svg-icons';
 import Navbar from './components/navbar';
 import Footer from './components/footer';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faUserGroup , faUserGraduate, faClock} from '@fortawesome/free-solid-svg-icons';
 import './assets/css/Forms.css';
 
 function UserRegister() {
@@ -10,18 +10,20 @@ function UserRegister() {
     <div className="container-forms">
       <Navbar />
       <div className="form-container">
-      <div className="img-forms">
-         src={require('./assets/img/logo2.png')} alt="Logo"
-         </div>
+      <img 
+          className="img-forms" 
+          src={require('./assets/img/logo2.png')} 
+          alt="Logo"
+        />
          <h2 className='title-form'>Registrar asistencia de estudiantes</h2>
         <form>
           <div className="form-group">
-            <FontAwesomeIcon icon={faUserGroup} className="icon" /> {/* Icono para cantidad de estudiantes */}
+          <FontAwesomeIcon icon={faUserGroup} fontSize={20} className='icons'></FontAwesomeIcon>
             <label htmlFor="cant">Cantidad de estudiantes:</label>
             <input type="text" id="cant" placeholder="Ingrese la cantidad de estudiantes" required />
           </div>
           <div className="form-group">
-            <FontAwesomeIcon icon={faUserGraduate} className="icon" /> {/* Icono para curso */}
+          <FontAwesomeIcon icon={faUserGraduate} fontSize={20} className='icons'></FontAwesomeIcon>
             <label htmlFor="course">Curso:</label>
             <select id="course" required>
               <option value="">Seleccione el curso</option>
@@ -29,7 +31,7 @@ function UserRegister() {
             </select>
           </div>
           <div className="form-group">
-            <FontAwesomeIcon icon={faClock} className="icon" /> {/* Icono para fecha */}
+          <FontAwesomeIcon icon={faClock} fontSize={20} className='icons'></FontAwesomeIcon>
             <label htmlFor="date">Fecha:</label>
             <input type="date" id="date" required />
           </div>
